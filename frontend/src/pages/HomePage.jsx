13ad8bb4ff6ae325d02423e84f29d65f428fdfc6
Svyatozar Naumov <svyatozar1@gmail.com>
2024-06-01 23:34:24 +0300

    added: homepage styling and contents

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/HomePage.css";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className="home-page-container">
+      <div className="home-container">
+        <h1 className="home-title">Добро пожаловать!</h1>
+        <p className="home-description">
+          Присоединяйтесь к нашему сообществу игроков и наслаждайтесь
+          увлекательными мероприятиями и другими активностями. Соревнуйтесь с
+          друзьями и другими студентами.
+        </p>
+        <div className="home-buttons">
+          <Link to="/user/register" className="home-button">
+            Регистрация
+          </Link>
+          <Link to="/user/login" className="home-button">
+            Вход
+          </Link>
+        </div>
+        <div className="home-buttons">
+          <Link to="/how-to" className="home-button">
+            Как играть
+          </Link>
+          <div className="home-ip">
+            <p>IP: server.ip.ru</p>
+          </div>
+        </div>
+      </div>
+      <div className="home-img-container">
+        <img className="home-img" src="minecraft.png" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
